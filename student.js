30150c713f1188d201aecfab1a16e4aa219ddf54
mkidushim <mkidushim@gmail.com>
2015-05-21 17:20:10 -0700

    Finished issue #1

--- student.js
+++ student.js
@@ -12,9 +12,9 @@ var high_array = [];
 function add_student() {
 
         var addstudent = Object.create(student_object);
-        addstudent.s_name = $('#student_name').val();
-        addstudent.s_course = $('#student_course').val();
-        addstudent.s_grade = parseFloat($('#student_grade').val());
+        addstudent.name = $('#student_name').val();
+        addstudent.course = $('#student_course').val();
+        addstudent.grade = parseFloat($('#student_grade').val());
         student_array.push(addstudent);
         //show_student();
         console.log(student_object);
@@ -40,15 +40,16 @@ function show_student() {
         });
         var name_o = $('<div>', {
             class: "st_name list-group-item",
-            text: student_array[i].s_name
+            text: student_array[i].name
         });
         var course_o = $('<div>', {
             class: "st_course list-group-item",
-            text: student_array[i].s_course
+            text: student_array[i].course
         });
+
         var grade_o = $('<div>', {
             class: "st_grade list-group-item",
-            text: student_array[i].s_grade
+            text: student_array[i].grade
         });
         var delete_o = $('<button>', {
             class: "delete list-group-item",
@@ -60,6 +61,7 @@ function show_student() {
             $(this).remove();
             student_array.splice(index,1);
             high_low_grade();
+
             highlight_low();
             highlight_high();
         })
@@ -75,7 +77,6 @@ function show_student() {
 $(document).ready(function() {
     $('.s_add').on('click', add_student);
 
-
 });
 
 function average_grade() 
@@ -86,7 +87,7 @@ function average_grade()
     console.log(student_array);
     for (var i = 0; i < student_array.length; i++) 
         {
-            sum += student_array[i].s_grade;
+            sum += parseFloat(student_array[i].grade);
 
         }   
     average = sum / student_array.length;
@@ -97,41 +98,47 @@ function average_grade()
 function high_low_grade() 
 {   
    
-    var high_grade = student_array[0].s_grade;
-    var low_grade = student_array[0].s_grade;
+    var high_grade = student_array[0].grade;
+    var low_grade = student_array[0].grade;
     var min = 0;
     var max = 0;
     for (var i = 0; i < student_array.length; i++) 
     {
-        if (student_array[i].s_grade == high_grade)
+        if (student_array[i].grade === high_grade)
         {
              high_array.push(i);
         }
-        if (student_array[i].s_grade > high_grade)
+        if (student_array[i].grade > high_grade)
         {
             high_array = [i];
-            high_grade = student_array[i].s_grade;
+            high_grade = student_array[i].grade;
         }
-        if (student_array[i].s_grade == low_grade)
+        if (student_array[i].grade === low_grade)
         {
              low_array.push(i);
         }
-        if (student_array[i].s_grade < low_grade)
+        if (student_array[i].grade < low_grade)
         {
             low_array = [i];
-            low_grade = student_array[i].s_grade;
+            low_grade = student_array[i].grade;
         }
     }
 }
 function highlight_low (){
     
     for (var i = 0; i < low_array.length; i++){
+        if ($('.st_grade').hasClass('low')){
+            return
+        }
         $('.st_grade').eq(low_array[i]).addClass('low')
         console.log('low')
     }
 }
 function highlight_high (){
     for (var i = 0; i < high_array.length; i++) {
+        if ($('.st_grade').hasClass('high')){
+            return
+        }
     $('.st_grade').eq(high_array[i]).addClass('high')
     console.log('high')
    
@@ -143,4 +150,18 @@ function highlight_high (){
 function footer () {
      $('.footer').append('<p class="col-sm-3 col-md-3">Pink = Highest Grade in the class.</p>')
          .append('<p class="col-sm-3 col-md-3">Blue = Lowest Grade in the class.</p>')
+}
+
+
+function get_student_data (){
+    $.ajax({
+        dataType: 'json',
+        url: 'http://s-apis.learningfuze.com/sgt/get',
+        success: function(response){
+            result = response;
+            console.log('Response: ', result)
+            student_array = student_array.concat(result.data)
+                    }
+
+    })
 }
