ef34e81ed1304e90f81dd6e3e61b02fcf81cac69
mkidushim <mkidushim@gmail.com>
2015-05-25 13:05:25 -0700

    Finished fixing highlight and get_average() functionality

--- student.js
+++ student.js
@@ -6,8 +6,9 @@ var student_object = {
     grade: null
 };
 var first_click = true;
-var low_array = [];
-var high_array = [];
+var low_array = [0];
+var high_array = [0];
+var data_inputed = true;
 
 function add_student() {
 
@@ -19,21 +20,23 @@ function add_student() {
         //show_student();
         console.log(student_object);
         console.log(student_array);
-        average_grade();
+
+
         //v--------KC Added the Average function to this button
-        
+
 
     }
     //got the show_student button to work we can also put it inside the add_student button by removing the comment before the show_student() function
 function show_student() {
 
     console.log('button worked')
-    
 
 
- footer();
+    average_grade();
+    footer();
 
     for (var i = 0; i < student_array.length; i++) {
+
         var output_stud = $('<div>', {
             class: "student_container list-group",
             data_index: i
@@ -56,112 +59,113 @@ function show_student() {
             type: "button",
             text: "delete"
         });
-        output_stud.on('click',function (){
+        output_stud.on('click', function() {
             var index = $(this).attr('data_index');
             $(this).remove();
-            student_array.splice(index,1);
+            student_array.splice(index, 1);
             high_low_grade();
-
             highlight_low();
+            high_low_grade();
             highlight_high();
         })
         $('#student_object').append(output_stud);
         $(output_stud).append(name_o, course_o, grade_o, delete_o);
 
     }
+    high_low_grade();
+    highlight_high();
+    high_low_grade();
+    highlight_low();
+
 
 };
 
 
 
 $(document).ready(function() {
-    $('.s_add').on('click', add_student);
+    $('body').on('click', '#add_btn', function() {
+        add_student();
+        if (data_inputed) {
+            get_student_data();
+            data_inputed = false;
+        }
+    });
+    $('body').on('click', '#show_btn', function() {
+        console.log('show button works')
+
+        show_student();
+    });
 
 });
 
-function average_grade() 
-{
+function average_grade() {
     var sum = 0;
     var average = 0;
     console.log(student_object);
     console.log(student_array);
-    for (var i = 0; i < student_array.length; i++) 
-        {
-            sum += parseFloat(student_array[i].grade);
+    for (var i = 0; i < student_array.length; i++) {
+        sum += parseFloat(student_array[i].grade);
 
-        }   
+    }
     average = sum / student_array.length;
-    console.log('Average: ',average);
+    console.log('Average: ', average);
     $('#student_display').val(average.toFixed(0));
 }
 
-function high_low_grade() 
-{   
-   
-    var high_grade = student_array[0].grade;
-    var low_grade = student_array[0].grade;
-    var min = 0;
-    var max = 0;
-    for (var i = 0; i < student_array.length; i++) 
-    {
-        if (student_array[i].grade === high_grade)
-        {
-             high_array.push(i);
-        }
-        if (student_array[i].grade > high_grade)
-        {
+function high_low_grade() {
+
+    var high_grade = parseFloat(student_array[0].grade);
+    var low_grade = parseFloat(student_array[0].grade);
+    for (var i = 0; i < student_array.length; i++) {
+        
+        if (parseFloat(student_array[i].grade) >= high_grade) {
             high_array = [i];
             high_grade = student_array[i].grade;
         }
-        if (student_array[i].grade === low_grade)
-        {
-             low_array.push(i);
-        }
-        if (student_array[i].grade < low_grade)
-        {
+    
+        if (parseFloat(student_array[i].grade) <= low_grade) {
             low_array = [i];
             low_grade = student_array[i].grade;
         }
     }
 }
-function highlight_low (){
-    
-    for (var i = 0; i < low_array.length; i++){
-        if ($('.st_grade').hasClass('low')){
-            return
-        }
+
+function highlight_low() {
+
+    for (var i = 0; i < low_array.length; i++) {
+
         $('.st_grade').eq(low_array[i]).addClass('low')
-        console.log('low')
+        console.log('low_array:', low_array)
     }
 }
-function highlight_high (){
+
+function highlight_high() {
     for (var i = 0; i < high_array.length; i++) {
-        if ($('.st_grade').hasClass('high')){
-            return
-        }
-    $('.st_grade').eq(high_array[i]).addClass('high')
-    console.log('high')
-   
-   
+
+        $('.st_grade').eq(high_array[i]).addClass('high')
+        console.log('high_array:', high_array)
+
+
     }
 
 
 }
-function footer () {
-     $('.footer').append('<p class="col-sm-3 col-md-3">Pink = Highest Grade in the class.</p>')
-         .append('<p class="col-sm-3 col-md-3">Blue = Lowest Grade in the class.</p>')
+
+function footer() {
+    $('.footer').append('<p class="col-sm-3 col-md-3">Pink = Highest Grade in the class.</p>')
+        .append('<p class="col-sm-3 col-md-3">Blue = Lowest Grade in the class.</p>')
 }
 
 
-function get_student_data (){
+function get_student_data() {
     $.ajax({
         dataType: 'json',
         url: 'http://s-apis.learningfuze.com/sgt/get',
-        success: function(response){
+        success: function(response) {
             result = response;
             console.log('Response: ', result)
             student_array = student_array.concat(result.data)
-                    }
+        }
 
     })
 }
